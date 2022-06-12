ac782ffce854c54819ab91b1e9f78cbf83249834
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-12 19:54:20 +0300

    commit tree 950146cfe93bbe46f86454028e608cb6f7466196

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const farmAnimals = 'cow horse sheep pig chicken';
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ')
+const [blackAndWhite, ,black, pink] = farmAnimals.split(' ')
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
 const muppet = {
@@ -46,3 +48,13 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+//Array
+let [red, orange, yellow, green, blue, indigo, violet] = colors
+let [r, o, y, g, b, ,v] = colors
+let[, , , , , indg] = colors
+
+//object
+let {muppetName, color, song, job, partner} = muppet
+let {song2,song4} = nestedMuppet.album.theMuppetMovie
+let {nestedJob, nestedPartner} = nestedMuppet
